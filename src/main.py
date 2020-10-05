c24d8176c55c1cdbd62467f818015f8bb35980e9
hernerd <55166743+hernerd@users.noreply.github.com>
2020-10-05 15:40:07 -0400

    Merged Changes (Shooting, Obsticles, Enemy, Powerup)

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import pygame
 import random
 
 from powerUp import MovementPowerUp
-from types import Player, Enemy
+from types import Player, Enemy, Obstacle
 
 filepath = os.path.dirname(__file__)
 
@@ -14,10 +14,24 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Dungeon Crawler")
 
 # Player
-playerImage = pygame.image.load(os.path.join(filepath, "assets/player.png"))
-player = Player(370, 480)
+playerX = 400
+playerY = 480
+player = Player(playerX, playerY)
 playerX_change = 0
 playerY_change = 0
+
+# crate
+crateX = 400
+crateY = 300
+crate = Obstacle(crateX, crateY)
+
+# player group
+player_group = pygame.sprite.Group()
+player_group.add(player)
+
+# obstacle group
+obstacle_group = pygame.sprite.Group()
+obstacle_group.add(crate)
 
 # Enemy
 enemyImage = pygame.image.load(os.path.join(filepath, "assets/bee.png"))
@@ -46,8 +60,8 @@ def displayPowerUp(powerUp):
     screen.blit(image, (powerUp.x, powerUp.y))
 
 
-def displayPlayer(x, y):
-    screen.blit(playerImage, (x, y))
+# def displayPlayer(x, y):
+#     screen.blit(playerImage, (x, y))
 
 def displayEnemy(x, y):
     screen.blit(enemyImage, (x, y))
@@ -67,6 +81,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
         if event.type == pygame.KEYDOWN:
 
             # Player Movement
@@ -83,19 +99,19 @@ while running:
             # Bullet Movement
             if bullet_state is "dead":
                 if event.key == pygame.K_UP:
-                    bulletX = player.playerX
+                    bulletX = playerX
                     direction_shot = "up"
                     fire_bullet(bulletX, bulletY)
                 if event.key == pygame.K_DOWN:
-                    bulletX = player.playerX
+                    bulletX = playerX
                     direction_shot = "down"
                     fire_bullet(bulletX, bulletY)
                 if event.key == pygame.K_LEFT:
-                    bulletX = player.playerX
+                    bulletX = playerX
                     direction_shot = "left"
                     fire_bullet(bulletX, bulletY)
                 if event.key == pygame.K_RIGHT:
-                    bulletX = player.playerX
+                    bulletX = playerX
                     direction_shot = "right"
                     fire_bullet(bulletX, bulletY)
 
@@ -105,17 +121,45 @@ while running:
             if event.key == pygame.K_w or event.key == pygame.K_s:
                 playerY_change = 0
 
-    player.playerX += playerX_change
-    if player.playerX <= 0:
-        player.playerX = 0
-    elif player.playerX >= 760:
-        player.playerX = 760
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 760:
+        playerX = 760
 
-    player.playerY += playerY_change
-    if player.playerY <= 0:
-        player.playerY = 0
-    elif player.playerY >= 560:
-        player.playerY = 560
+    playerY += playerY_change
+    if playerY <= 0:
+        playerY = 0
+    elif playerY >= 560:
+        playerY = 560
+
+    if pygame.sprite.collide_mask(player, crate):
+        if playerX_change < 0:
+            while pygame.sprite.collide_mask(player, crate):
+                playerX_change = .1
+                playerX += playerX_change
+                player.pos(playerX, playerY)
+            playerX_change = 0
+        elif playerX_change > 0:
+            while pygame.sprite.collide_mask(player, crate):
+                playerX_change = -.1
+                playerX += playerX_change
+                player.pos(playerX, playerY)
+            playerX_change = 0
+        if playerY_change < 0:
+            while pygame.sprite.collide_mask(player, crate):
+                playerY_change = .1
+                playerY += playerY_change
+                player.pos(playerX, playerY)
+            playerY_change = 0
+        elif playerY_change > 0:
+            while pygame.sprite.collide_mask(player, crate):
+                playerY_change = -.1
+                playerY += playerY_change
+                player.pos(playerX, playerY)
+            playerY_change = 0
+
+    player.pos(playerX, playerY)
 
     #Enemy Movement
     enemy.enemyX += enemyX_change
@@ -131,11 +175,12 @@ while running:
         enemyY_change = -0.1
 
     #Checking Enemy Collisions 
-    if enemy.enemyX - 24 <= player.playerX <= enemy.enemyX + 24 and enemy.enemyY - 24 <= player.playerY <= enemy.enemyY + 24:
+    if enemy.enemyX - 24 <= playerX <= enemy.enemyX + 24 and enemy.enemyY - 24 <= playerY <= enemy.enemyY + 24:
             pygame.quit
+
     # Bullet Moving
     if bulletY <= 0 or bulletY >= 600 or bulletX <= 0 or bulletX >= 800:
-        bulletY = player.playerY
+        bulletY = player.y
         bullet_state = "dead"
     if bullet_state is "live":
         fire_bullet(bulletX, bulletY)
@@ -150,7 +195,7 @@ while running:
 
     # check player contacting powerUp
     for powerUp in powerUpsOnScreen:
-        if powerUp.x - 24 <= player.playerX <= powerUp.x + 24 and powerUp.y - 24 <= player.playerY <= powerUp.y + 24:
+        if powerUp.x - 24 <= playerX <= powerUp.x + 24 and powerUp.y - 24 <= playerY <= powerUp.y + 24:
             powerUp.applyPlayerEffect(player)
             powerUpsOnScreen.remove(powerUp)
             powerUpsInEffect.append(powerUp)
@@ -165,6 +210,7 @@ while running:
             powerUpsInEffect.remove(powerUp)
 
     displayEnemy(enemy.enemyX, enemy.enemyY)
-    displayPlayer(player.playerX, player.playerY)
+    obstacle_group.draw(screen)
+    player_group.draw(screen)
  
     pygame.display.update()
--- src/types.py
+++ src/types.py
@@ -1,10 +1,25 @@
-class Player:
 
-    def __init__(self, x, y):
-        self.playerX = x
-        self.playerY = y
-        self.xSpeed = 0.1
-        self.ySpeed = 0.1
+import pygame
+import os.path
+
+filepath = os.path.dirname(__file__)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(filepath, "assets/player.png")).convert_alpha()
+        self.x = x_pos
+        self.y = y_pos
+        self.rect = self.image.get_rect()
+        self.rect.center = [x_pos, y_pos]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.xSpeed = 0.3
+        self.ySpeed = 0.3
+
+    def pos(self, x_pos, y_pos):
+        self.rect.center = [x_pos, y_pos]
+        self.x = x_pos
+        self.y = y_pos
 
 class Enemy:
 
@@ -12,3 +27,13 @@ class Enemy:
         self.enemyX = x
         self.enemyY = y
        
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(filepath, "assets/crate.png")).convert_alpha()
+        self.x = pos_x
+        self.y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.center = [pos_x, pos_y]
+        self.mask = pygame.mask.from_surface(self.image)
