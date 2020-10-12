898b400829f208857e92788437bd9932b9438b5d
elinderm <48486820+elinderm@users.noreply.github.com>
2020-10-12 19:24:24 -0400

    Update classes.py
    added health to player and enemy, a couple animations, bullets deal damage to enemy

--- src/classes.py
+++ src/classes.py
@@ -6,15 +6,17 @@ filepath = os.path.dirname(__file__)
 
 
 class Player(pygame.sprite.Sprite):
-    images = []
+    idle_images = []
+    right_walk = []
+    i_frame = 0
+    r_frame = 0
     moving = False
 
     def __init__(self, x_pos, y_pos):
         pygame.sprite.Sprite.__init__(self)
         self.load_images()
         self.name = "player"
-        self.image = self.images[0]
-        self.frame = 0
+        self.image = self.idle_images[0]
         self.x = x_pos
         self.y = y_pos
         self.rect = self.image.get_rect()
@@ -24,35 +26,38 @@ class Player(pygame.sprite.Sprite):
         self.ySpeed = 5
         self.x_change = 0
         self.y_change = 0
+        self.health = 100
 
     def load_images(self):
-        i = 0
         img_x = 0
         img_y = 0
         player_sheet = SpriteSheet("assets/player_spritesheet.png")
-        for i in range(9):
-            for j in range(5):
-                self.images.append(player_sheet.get_image(img_x, img_y, 50, 37))
+        player_walk = SpriteSheet("assets/player_walk.png")
+        for i in range(10):
+            for j in range(6):
+                self.idle_images.append(player_sheet.get_image(img_x, img_y, 50, 37))
             img_x += 50
-            i += 1
+        img_x = 0
+        for i in range(8):
+            for j in range(6):
+                self.right_walk.append(player_walk.get_image(img_x, img_y, 50, 37))
+            img_x += 50
 
     def pos(self, x_pos, y_pos):
         if not self.moving:
-            self.image = self.images[self.frame]
+            self.image = self.idle_images[self.i_frame]
             self.mask = pygame.mask.from_surface(self.image)
-            # print("Frames: ", self.frame)
-            self.frame += 1
-            if self.frame >= 45:
-                self.frame = 0
+            # print("Frames: ", self.i_frame)
+            self.i_frame += 1
+            if self.i_frame >= 60:
+                self.i_frame = 0
         if self.moving:
-            self.image = self.images[0]
-            self.frame = 0
+            self.image = self.right_walk[self.r_frame]
             self.mask = pygame.mask.from_surface(self.image)
-        self.rect.center = [x_pos, y_pos]
-        self.x = x_pos
-        self.y = y_pos
-
-    def pos(self, x_pos, y_pos):
+            # print("Frames: ", self.r_frame)
+            self.r_frame += 1
+            if self.r_frame >= 48:
+                self.r_frame = 0
         self.rect.center = [x_pos, y_pos]
         self.x = x_pos
         self.y = y_pos
@@ -68,8 +73,10 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.mask = pygame.mask.from_surface(self.image)
-        self.x_change = .3
-        self.y_change = .3
+        self.x_change = 10
+        self.y_change = 10
+        self.health = 10
+        self.damage = 10
 
     def pos(self, x, y):
         self.rect.center = [x, y]
@@ -87,19 +94,39 @@ class Obstacle(pygame.sprite.Sprite):
         self.rect.center = [pos_x, pos_y]
         self.mask = pygame.mask.from_surface(self.image)
 
+
 class Bullet(pygame.sprite.Sprite):
+    images = []
+
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
+        self.load_images()
         self.name = "bullet"
-        self.image = pygame.image.load(os.path.join(filepath, "assets/projectile.png")).convert_alpha()
+        # self.image = pygame.image.load(os.path.join(filepath, "assets/projectile.png")).convert_alpha()
+        self.image = self.images[0]
+        self.frame = 0
         self.x = x
         self.y = y
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.mask = pygame.mask.from_surface(self.image)
         self.direction = ""
+        self.damage = 5
+
+    def load_images(self):
+        img_x = 0
+        img_y = 0
+        bullet_sheet = SpriteSheet("assets/projectile_spritesheet.png")
+        for i in range(2):
+            for j in range(10):
+                self.images.append(bullet_sheet.get_image(img_x, img_y, 16, 16))
+            img_x += 16
 
     def pos(self, x, y):
+        if self.frame >= 20:
+            self.frame = 0
+        self.image = self.images[self.frame]
+        self.frame += 1
         self.rect.center = [x, y]
         self.x = x
         self.y = y
